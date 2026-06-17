d8e90510c1f96ff5bf37d1aa3be9636a0002af7c
muhammed sahal <muhammedsahalap717@gmail.com>
2026-06-17 22:36:04 +0530

    style: update ProjectGrid layout container width and responsive padding

--- components/ProjectGrid.tsx
+++ components/ProjectGrid.tsx
@@ -169,7 +169,7 @@ const ProjectCard = ({ project, index }: { project: any; index: number }) => {
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default function ProjectGrid({ projects }: { projects: any[] }) {
   return (
-    <div className="relative z-10 mx-auto flex w-full max-w-screen-2xl flex-col overflow-hidden px-4 py-20 md:px-8 lg:py-40 xl:px-12">
+    <div id="projects" className="relative z-10 mx-auto flex w-full max-w-[100rem] flex-col overflow-hidden px-6 py-20 lg:px-16 lg:py-40">
       {/* Decorative background grid */}
       <div className="pointer-events-none absolute inset-0 -z-10 bg-[linear-gradient(to_right,#80808008_1px,transparent_1px),linear-gradient(to_bottom,#80808008_1px,transparent_1px)] bg-[size:4rem_4rem]" />
 
