0921283efc194a2844407f5c61cf904195103012
muhammed sahal <muhammedsahalap717@gmail.com>
2026-07-01 00:07:01 +0530

    feat: replace legacy favicon assets with new SVG icon in site metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -100,8 +100,8 @@ export const metadata: Metadata = {
   },
 
   icons: {
-    icon: "../favicon.ico",
-    shortcut: "/favicon-32x32.png",
+    icon: "/icon.svg",
+    shortcut: "/icon.svg",
     apple: "/apple-touch-icon.png",
   },
 };
